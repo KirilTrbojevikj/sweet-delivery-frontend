c20b58fe219f137fe1015fee11c9ad2990d681c0
KirilTrbojevikj <62806905+KirilTrbojevikj@users.noreply.github.com>
2022-06-28 19:41:56 +0200

    Add files via upload

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -78,16 +78,12 @@ export const LoginPage: React.FC = () => {
           </div>
         </div>
       </div>
-      <div id="logo" className="hidden lg:block relative w-0 flex-1">
-        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div className="logo-holder logo-4">
-              <h3>Sweet</h3>
-              <h5>Delivery</h5>
-              <p>Deliciousness to your front door</p>
-            </div>
-          </div>
-        </div>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="/loginbg.jpg"
+          alt=""
+        />
       </div>
     </div>
   );
